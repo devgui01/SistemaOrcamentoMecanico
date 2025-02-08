69566ca61a729662562196309dc55f5a37e2e462
Guilherme Kauan <123951833+devgui01@users.noreply.github.com>
2025-02-08 12:56:49 -0400

    commit 08/02/255

--- cars/forms.py
+++ cars/forms.py
@@ -8,12 +8,12 @@ class CarModelForm(forms.ModelForm):
 
     def clean_value(self):
         value = self.cleaned_data.get('value')
-        if value < 150000:
-            self.add_error('value', 'Valor mínimo do veículo deve ser de R$150.000')
+        if value < 5:
+            self.add_error('value', 'Valor mínimo da peça é de R$5.0 ')
         return value
     
     def clean_factory_year(self):
         factory_year = self.cleaned_data.get('factory_year')
         if factory_year < 2000:
-            self.add_error('factory_year', 'Não é possível cadastrar veículos com ano inferior a 2000')
+            self.add_error('factory_year', 'Sistema não permite cadastrar peças com ano inferior a 2000')
         return factory_year
